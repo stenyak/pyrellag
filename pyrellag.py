ff7eb1b52d9a4b116e13aeb2f03d9199a468c6d8
Bruno Gonzalez <stenyak@stenyak.com>
2013-09-23 23:13:06 +0200

    Add page generation time.

--- pyrellag.py
+++ pyrellag.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Copyright (c) 2013, Bruno Gonzalez <stenyak@stenyak.com>. This software is licensed under the Affero General Public License version 3 or later.  See the LICENSE file.
 
-import os, urllib
+import os, urllib, time
 from flask import Flask, redirect, send_file, render_template
 from gallery import Gallery
 app = Flask(__name__)
@@ -16,10 +16,14 @@ def root():
 
 @app.route('/gallery/<path:path>')
 def show_gallery(path):
+    start = time.time()
     path = urllib.unquote(path).encode("utf-8")
     g = Gallery(path)
     g.populate()
-    return render_template("gallery.html", path = g.path.decode("utf-8"), parents = g.get_parents(), basename = os.path.basename(g.path.decode("utf-8")), nfiles = len(g.files), galleries = g.get_galleries(), files = g.get_files())
+    result = render_template("gallery.html", path = g.path.decode("utf-8"), parents = g.get_parents(), basename = os.path.basename(g.path.decode("utf-8")), nfiles = len(g.files), galleries = g.get_galleries(), files = g.get_files())
+    end = time.time()
+    result = result.replace("TTTTIME", "%.4f" %(end-start))
+    return result
 
 @app.route('/file/<path:path>')
 def show_image(path):
