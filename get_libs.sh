3663114be056fbc442317a8aeaef163c19c4d6d1
Bruno Gonzalez <stenyak@stenyak.com>
2013-12-10 23:20:12 +0100

    Add script to install dependencies.

---
+++ get_libs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright 2012 Bruno Gonzalez
+# This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
+set -e
+pip install --user flask
+pip install --user Flask-Login
+pip install --user Flask-OpenID
+pip install --user sqlalchemy
